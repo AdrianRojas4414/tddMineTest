f6d9829a12044567a273413826ffaf9b900b4899
adrian <adrian.rojas.o@gmail.com>
2025-09-29 20:45:50 -0400

    primer commit

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,8 +2,10 @@ import sumar from "./sumador.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
+    expect(sumar(3, 2)).toEqual(6);
   });
+
+  
 });
 
 

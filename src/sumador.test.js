9f20ab4e99af63374038cb02ce921abb7c888e23
adrian <adrian.rojas.o@gmail.com>
2025-09-29 20:52:55 -0400

    tercer commit

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -8,6 +8,16 @@ describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
+
+  it("deberia sumar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(5);
+  });
+
+  it("deberia sumar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(6);
+  });
+
+  
 });
 
 
